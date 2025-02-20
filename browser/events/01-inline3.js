e0620f80e065efe68b96d7ce427e6ecfbcda9f8d
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-02-20 15:43:06 +0200

    minor modifications in 01-inline3.js

--- browser/events/01-inline3.js
+++ browser/events/01-inline3.js
@@ -1,4 +1,4 @@
 const btn = document.getElementById("btn")
 btn.addEventListener("click", function() {
-    alert("Hello World!", false)    // useCapture
-})
+    alert("Hello World!")
+}, false)   // event propagation: bubbling - capturing set to "false" by default
