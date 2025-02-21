6ce3a2fba0ffb0cd0eafccf62c477c1e1a0ed64d
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-02-21 13:23:31 +0200

    added final object challenge for chapter 1

--- challenges/01d-objects.js
+++ challenges/01d-objects.js
@@ -8,13 +8,64 @@ const obj = {
     location: "Athens"
 }
 
-const arr = [obj['firstname'], obj['age'], obj['location']]
+const arr = ['firstname','age','location']
 
 function selectProperties(obj, arr) {
+    const newObj = {}
     for (let i = 0; i < arr.length; i++) {
-        const newObj = {}
+        const key = arr[i]
+        if (obj.hasOwnProperty(key)) {
+            newObj[key] = obj[key]
+        }
     }
     return newObj
 }
 
 console.log(selectProperties(obj, arr))
+
+// 2
+
+function mapping(obj) {
+  let keys = Object.keys(obj).map((key) => key.toUpperCase());
+  let values = Object.values(obj).map((value) => {
+      if (typeof value === "string") {
+        return value.toUpperCase();
+      } else {
+        return value
+    }
+  });
+
+  let transformedObj = {};
+  keys.forEach((key, index) => {
+    transformedObj[key] = values[index]
+  })
+
+  return transformedObj;
+};
+
+function transformObj(obj, mapping) {
+    return mapping(obj)
+}
+
+console.log(transformObj(obj, mapping));
+
+// 3
+
+function modifyObj(obj, callback) {
+    let modifiedObj = {};
+
+    for (let key in obj) {
+        modifiedObj[key] = callback(key, obj[key])
+    }
+
+    return modifiedObj;
+}
+
+function modifyValue(key, value) {
+    if (typeof value === 'string') {
+        return value.toLowerCase()
+    } else return value
+}
+
+const anotherObj = modifyObj(obj, modifyValue)
+console.log(anotherObj)
